abf544ab16f19787a7ababc4ba29f282bf5408e1
koashi <koashi_dev@yahoo.com>
2014-04-07 22:44:20 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,4 +9,35 @@ CURRENT=$(cd $(dirname $0) && pwd)
 ln -s ${CURRENT}/.bash_profile ~
 ln -s ${CURRENT}/.bashrc ~
 ln -s ${CURRENT}/.vimrc ~
+ln -s ${CURRENT}/.gvimrc ~
 ln -s ${CURRENT}/.tmux.conf ~
+
+# Download git-completion and git-prompt
+if [ -x /usr/bin/curl ]; then
+    curl -o ~/.git-completion.bash https://raw.github.com/git/git/master/contrib/completion/git-completion.bash
+    curl -o ~/.git-prompt.sh https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh
+else
+    echo 'curl not found'
+fi
+
+if [ -x /usr/bin/git ]; then
+    # Install NeoBundle.vim
+    if [ ! -d ~/.vim/bundle ]; then
+        mkdir -p ~/.vim/bundle
+        git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+    fi
+
+    # git
+    if [ ! -f ~/.gitconfig ]; then
+        git config --global core.editor vim
+        git config --global alias.st status
+        git config --global alias.ci commit
+        git config --global alias.co checkout
+        git config --global alias.br branch
+        git config --global color.status auto
+        git config --global color.diff auto
+        git config --global color.branch auto
+    fi
+else
+    echo 'git not found'
+fi
